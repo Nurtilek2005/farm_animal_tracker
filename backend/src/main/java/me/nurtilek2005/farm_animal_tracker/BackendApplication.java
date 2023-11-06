7d25b39ea66225c0d914287d7bb2e70e04336beb
Nurtilek Sydykovich <nurtileks2005@gmail.com>
2023-11-06 18:04:05 +0600

    client and backend initialized

---
+++ backend/src/main/java/me/nurtilek2005/farm_animal_tracker/BackendApplication.java
@@ -0,0 +1,13 @@
+package me.nurtilek2005.farm_animal_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/me/nurtilek2005/farm_animal_tracker/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nurtilek2005.farm_animal_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
